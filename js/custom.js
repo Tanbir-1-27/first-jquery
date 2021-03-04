53366a15560f2830fdf47619a56f4b042ed23893
Your Name <tanbirhaque53@gmail.com>
2021-03-05 01:02:26 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+$('#btn').click(function () {
+    $('.box .mini-box').animate({
+        left:'350px',
+    });
+
+    $('.box .mini-box').animate({
+        // left:'350px',
+        top: '350px',
+    });
+
+    $('.box .mini-box').animate({
+        left: '0px',
+    });
+
+    $('.box .mini-box').animate({
+        top: '0px',
+    });
+
+    $('.box .mini-box').animate({
+        left: '175px',
+        top: '175px',
+        borderRadius: '50%',
+    });
+
+    $('.box .mini-box').animate({
+        left: '350px',
+        top: '350px',
+        borderRadius: '0%',
+    });
+
+    $('.box .mini-box').animate({
+        left: '0px',
+    });
+
+    $('.box .mini-box').animate({
+        top: '0px',
+    });
+});
